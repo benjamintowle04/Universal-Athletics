4b722d525c6ac6934251070b90d0ab3ef8b837fc
benjamintowle04 <benjamintowle04@gmail.com>
2024-04-08 15:34:47 -0500

    UPDATE 2/8/24

--- coach_spotlight.js
+++ coach_spotlight.js
@@ -21,7 +21,6 @@ var dataId = getUrlParameter('id');
 // You can now use the dataId variable as needed
 console.log('Data ID:', dataId);
 
-
 //Fetch the data for the particular coach
 fetch ('./coach_data.json') 
 .then (response => response.json())
